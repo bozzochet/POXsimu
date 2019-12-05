09d17150e3739a1451c42ad9f8e4503d74bb0a0a
Matteo Duranti <bozzo1983@gmail.com>
2019-12-05 12:23:57 +0100

    some modification in the logic: still not working well

--- Analysis/DAQ.C
+++ Analysis/DAQ.C
@@ -12,86 +12,101 @@ Double_t eDep[nHitsMax]={0.};
 
 ///////////////////////////////////////////////////////////////////
 
-void TrackFinding( vector<vector<double>> hcoord,vector<vector<double>> &dir,int iter=1){ 
+void TrackFinding( vector<vector<double>> hcoord, vector<vector<double>> &dir, string name, bool kDraw=true){
 
-    int n_search=2;               //number of track to search
-    if (iter>n_search)return;
-    int nHits=hcoord.size();
+  static int iter=0;
+  iter++;
+  
+  int n_search=2;               //number of track to search
+  if (iter>n_search) return;
+  int nHits=hcoord.size();
 
-    //this 2D histogram will contain the Hough transform
-    int binth=100,minth=-2,maxth=2;  //binning of theta
-    int binr=100,minr=-3,maxr=3;     //binning of r
-    //double scartoth=0;//double(maxth-minth)/double(2*binth);
-    //double scartor=0;//double(maxr-minr)/double(2*binr);
-    TH2D *h=new TH2D("h","hough",binth,minth,maxth,  binr,minr,maxr);
+  TCanvas* c2 = new TCanvas(Form("%s_%d", name.c_str(), iter), "Hough Transform");
+  
+  //this 2D histogram will contain the Hough transform
+  int binth=500; double minth=-TMath::Pi(),maxth=TMath::Pi();  //binning of theta
+  int binr=500; double minr=-3, maxr=3;     //binning of r
+  //double scartoth=0;//double(maxth-minth)/double(2*binth);
+  //double scartor=0;//double(maxr-minr)/double(2*binr);
+  TH2D* h = new TH2D(Form("h_%s_%d", name.c_str(), iter), "hough", binth, minth, maxth, binr, minr, maxr);
 
-    //Generate the Hough transform
-    double 
-        m,  //la pendenza 
-        r,  //la distanza tra retta e origine
-        th; //l'angolo tra r e l'asse z
-    for (int i=0;i<nHits;i++){
-        for (int j=i+1;j<nHits;j++){
-            if(hcoord[i][0]!=hcoord[j][0]){
-                m=(hcoord[i][1]-hcoord[j][1])/(hcoord[i][0]-hcoord[j][0]);
-                th=atan(m);
-                r=cos(th)*hcoord[i][1]-sin(th)*hcoord[i][0];
-                h->Fill(th,r);
-            }
-        }
+  //Generate the Hough transform
+  double 
+    r,  //la distanza tra retta e origine
+    th; //l'angolo tra r e l'asse z
+  for (int i=0;i<nHits;i++){
+    for (int j=i+1;j<nHits;j++){
+      if(hcoord[i][0]!=hcoord[j][0]){
+	//	sono un coglione: era meglio m che gia' è univocamente definita in segno
+	th=atan2((hcoord[i][1]-hcoord[j][1]), (hcoord[i][0]-hcoord[j][0]));
+	r=cos(th)*hcoord[i][1]-sin(th)*hcoord[i][0];
+	printf("%f %f\n", th, r);
+	h->Fill(th,r);
+      }
     }
+  }
 
-    //Search for peaks in the Hough transform
-    TSpectrum2 *s=new TSpectrum2();
-    int n_found=s->Search(h,1,"nobackground",.5);
+  //Search for peaks in the Hough transform
+  TSpectrum2 *s=new TSpectrum2();
+  int n_found=s->Search(h,1,"nobackground",.5);
   //  s->Print();
-    double *thpeaks=s->GetPositionX();
-    double *rpeaks=s->GetPositionY();
-   // cout<<s->GetNPeaks()<<endl;
-    if(s->GetNPeaks()==0) return;
+  double *thpeaks=s->GetPositionX();
+  double *rpeaks=s->GetPositionY();
+  // cout<<s->GetNPeaks()<<endl;
+  if(s->GetNPeaks()==0) {
+    if (c2 && !kDraw) delete c2;
+    if (h) delete h;
+    iter--;
+    return;
+  }
 
-    //Add the best track (x0,theta) to the OUTPUT vector
-    vector<double> best_dir(2);
-    best_dir[0]=thpeaks[0]; //angle
-    best_dir[1]= rpeaks[0] / cos(best_dir[0]);  //q
-    dir.push_back(best_dir);
-/*
+  //Add the best track (x0,theta) to the OUTPUT vector
+  vector<double> best_dir(2);
+  best_dir[0]=thpeaks[0]; //angle
+  best_dir[1]= rpeaks[0] / cos(best_dir[0]);  //q
+  dir.push_back(best_dir);
+
+  if (kDraw) {
     //Plot the transform
-    auto *c2=new TCanvas("c2","Hough Transform");
     h->SetXTitle("angolo");
     h->SetYTitle("distanza");
-    h->Draw("lego2");
-    TString filename=Form("%s_%d%s","Immagini/hough",iter,".png");
+    //    h->DrawCopy("lego2");
+    h->DrawCopy("colz");
+    TString filename=Form("%s_%d%s", name.c_str(), iter, ".png");
     c2->SaveAs(filename);
-    delete c2;
-*/    delete h;
+  }
+  if (c2 && !kDraw) delete c2;
+  if (h) delete h;
 
-    //remove fitted points
-    vector<vector<double>> newcoord;
-    int z0=10000;int i0=0;double sum=0;double conta=0;
-    for (int i=0;i<nHits;i++){
-        double xexp=best_dir[1]+tan(best_dir[0])*hcoord[i][0];
-        double chi=pow(hcoord[i][1]-xexp,2);
-        if(chi>1*abs(xexp)){
-            newcoord.push_back(hcoord[i]);
-        }
-        else {sum=sum+chi;conta++;}
-        if(hcoord[i][0]<z0){
-            z0=hcoord[i][1];
-            i0=i;
-        }
+  //remove fitted points
+  vector<vector<double>> newcoord;
+  int z0=10000;int i0=0;double sum=0;double conta=0;
+  for (int i=0;i<nHits;i++){
+    double xexp=best_dir[1]+tan(best_dir[0])*hcoord[i][0];
+    double chi=pow(hcoord[i][1]-xexp,2);
+    if(chi>1*abs(xexp)){
+      newcoord.push_back(hcoord[i]);
     }
-    newcoord.push_back(hcoord[i0]);
+    else {sum=sum+chi;conta++;}
+    if(hcoord[i][0]<z0){
+      z0=hcoord[i][1];
+      i0=i;
+    }
+  }
+  newcoord.push_back(hcoord[i0]);
 
-    //Add chi squared to the output vector
-    //chisq.push_back(sum/conta);
+  //Add chi squared to the output vector
+  //chisq.push_back(sum/conta);
+  
+  TrackFinding(newcoord,dir,name, kDraw);
 
-    TrackFinding(newcoord,dir,iter+1);
+  iter--;
+  return;
 }
 
 //////////////////////////////////////////////////////////////
 
-void PlotHits(vector<vector<Double_t>> hcoord,vector<vector<Double_t>> dir,string name,int ev, int evID, bool last=true){
+void PlotHits(vector<vector<Double_t>> hcoord,vector<vector<Double_t>> dir,string name,int ev, int evID, bool kDraw=true){
   
   int n=hcoord.size();
 
@@ -103,33 +118,34 @@ void PlotHits(vector<vector<Double_t>> hcoord,vector<vector<Double_t>> dir,strin
   }
 
   int n_found=dir.size();
-  TF1 *f[n_found];
+  printf("%s) n dir found %d\n", name.c_str(), n_found);
+  TF1* f[n_found];
   for(int i=0;i<n_found;i++){
-      f[i]=new TF1("f1","[0]*x+[1]",-30,30);
-      f[i]->SetParameter(0,tan(dir[i][0]));
-      f[i]->SetParameter(1,dir[i][1]);
-    }
+    f[i]=new TF1("f1","[0]*x+[1]",-30,30);
+    f[i]->SetParameter(0,tan(dir[i][0]));
+    f[i]->SetParameter(1,dir[i][1]);
+  }
 
   TGraph *g=new TGraph(n,z,x);
-  TCanvas *c=new TCanvas(Form("%s_%s", name.c_str(), "Hits2D"),"Hits");
+  TCanvas *c=new TCanvas(Form("2D_%s_%s", name.c_str(), "Hits2D"),"Hits2D");
 
   //plot the hits
   g->SetMarkerStyle(3);
   g->Draw("AP");
   for(int i=0;i<n_found;i++)
-      f[i]->Draw("SAME");
+    f[i]->Draw("SAME");
   TString filename = Form("%s_%d_%d%s", name.c_str(), ev, evID, ".png");
   c->SaveAs(filename);
 
-  if (!last) delete c;
+  if (!kDraw) delete c;
 }
 
 ///////////////////////////////////////////////////////////////////////
 
-void PlotHits3D(Double_t x[],Double_t y[],Double_t z[],int n,string name,int ev, int evID, bool last=true){
+void PlotHits3D(Double_t x[],Double_t y[],Double_t z[],int n,string name,int ev, int evID, bool kDraw=true){
 
   TGraph2D *g=new TGraph2D(nTotalHits,x,y,z);
-  TCanvas *c=new TCanvas(Form("%s_%s", name.c_str(), "Hits3D"),"Hits");
+  TCanvas *c=new TCanvas(Form("3D_%s_%s", name.c_str(), "Hits3D"),"Hits3D");
 
   //plot the hits
   g->SetMarkerStyle(3);
@@ -137,7 +153,7 @@ void PlotHits3D(Double_t x[],Double_t y[],Double_t z[],int n,string name,int ev,
   TString filename = Form("%s_%d_%d%s", name.c_str(), ev, evID, ".png");
   c->SaveAs(filename);
   
-  if (!last) delete c;
+  if (!kDraw) delete c;
 }
 
 /////////////////////////////////////////////////////////////
@@ -182,75 +198,49 @@ void DAQ(int reqEv=-9999){
   vector<vector<Double_t>> traccey;
 
   bool last=false;
-  
-  if (reqEv==-9999) {
-    //The for loop run through every event in the tree
-    for (int Ev=0; Ev<nEvs; Ev++){
-      
-      if (Ev==nEvs-1) last=true;
-      
-      clear();
-      zx.clear();
-      zy.clear();
 
-      traccex.clear();
-      traccey.clear();
-      
-      T->GetEntry(Ev);
-      
-      //this loop fill the vectors of vectors from the tree branches
-      for (int i=0;i<nTotalHits;i++){
-	      if(eDep[i]>.01 && zCoord[i]<30){    //we're taking into consideration only the detectable hits in the tracker
-    	    vector<Double_t> hitx(2);
-      	  hitx[0]=zCoord[i];
-      	  hitx[1]=xCoord[i];
-	        zx.push_back(hitx);
-
-    	    vector<Double_t> hity(2);
-      	  hity[0]=zCoord[i];
-      	  hity[1]=yCoord[i];
-      	  zy.push_back(hity);
-	      }
-      }
-      if (zx.size()<3 && zy.size()<3) continue;
-      TrackFinding(zx,traccex);
-      TrackFinding(zy,traccey);
-      PlotHits(zx,traccex,"Immagini/simuZX",Ev, evID, last);
-      PlotHits(zy,traccey,"Immagini/simuZY",Ev, evID, last);
-    }
-  } else {
-
-    last=true;
+  //The for loop run through every event in the tree
+  int startEv=0;
+  int stopEv=nEvs-1;
+  if (reqEv>=0) {
+    startEv=reqEv;
+    stopEv=reqEv;
+  }
+  for (int Ev=startEv; Ev<=stopEv; Ev++){
+    
+    if (Ev==stopEv) last=true;
     
     clear();
     zx.clear();
     zy.clear();
-
+    
     traccex.clear();
     traccey.clear();
     
-    T->GetEntry(reqEv);
+    T->GetEntry(Ev);
+
+    if (reqEv>=0) PlotHits3D(zCoord,xCoord,yCoord,nTotalHits,"Immagini/simu",Ev, evID, last);
     
-    PlotHits3D(zCoord,xCoord,yCoord,nTotalHits,"Immagini/simu",reqEv, evID, last);
-   
     //this loop fill the vectors of vectors from the tree branches
     for (int i=0;i<nTotalHits;i++){
-      if(eDep[i]>.01 && zCoord[i]<30){
-	    vector<Double_t> hitx(2);
-	    hitx[0]=zCoord[i];
-	    hitx[1]=xCoord[i];
-	    zx.push_back(hitx);
+      if(eDep[i]>.01 && zCoord[i]<30){    //we're taking into consideration only the detectable hits in the tracker
+	vector<Double_t> hitx(2);
+	hitx[0]=zCoord[i];
+	hitx[1]=xCoord[i];
+	zx.push_back(hitx);
 	
-    	vector<Double_t> hity(2);
-	    hity[0]=zCoord[i];
-	    hity[1]=yCoord[i];
-	    zy.push_back(hity);
+	vector<Double_t> hity(2);
+	hity[0]=zCoord[i];
+	hity[1]=yCoord[i];
+	zy.push_back(hity);
       }
     }
-    TrackFinding(zx,traccex);
-    TrackFinding(zy,traccey);
-    PlotHits(zx,traccex,"Immagini/simuZX",reqEv, evID, last);
-    PlotHits(zy,traccey,"Immagini/simuZY",reqEv, evID, last);
+    if (zx.size()<3 && zy.size()<3) continue;
+    TrackFinding(zx,traccex, "Immagini/houghZX", last);
+    TrackFinding(zy,traccey, "Immagini/houghZY", last);
+    PlotHits(zx,traccex,"Immagini/simuZX",Ev, evID, last);
+    PlotHits(zy,traccey,"Immagini/simuZY",Ev, evID, last);
   }
 
+  return;
 }
